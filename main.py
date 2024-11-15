37762faf48846740f7a78f57a5914336f605b73f
jokil123 <jo.kil.plays@gmail.com>
2024-11-15 18:38:53 +0100

    working

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-from PIL import Image
-import matplotlib
-import matplotlib.pyplot as plt
+import PIL.Image
+import cv2
+import PIL
 import numpy as np
+import matplotlib.pyplot as plt
 
-def main():
-    # img = np.asarray(Image.open("./input.png"))
-    # imgplot = plt.imshow(img)
-    # print(imgplot)
-    # imgplot.show()
-    pass
-    
+img = PIL.Image.open("./test_01.psd")
+mono_image = img.convert("L")
+img_array = np.asarray(mono_image)
 
-if __name__ == "__main__":
-    main()
+pixel_count = img_array.shape[0] * img_array.shape[1]
+
+xS = 0
+yS = 0
+
+a = 0
+
+for iy, ix in np.ndindex(img_array.shape):
+    v = img_array[iy, ix] / 255 # Density 0-1
+    # v = 1
+    a += v
+    xS += ix*v
+    yS += iy*v
+
+x = xS / a
+y = yS / a
+
+
+plt.imshow(img)
+plt.plot(x,y, "ro")
+
+plt.show()
