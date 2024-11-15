ab5d388524c7df5337e1b1c8920ad68f65c9703c
jokil123 <jo.kil.plays@gmail.com>
2024-11-15 17:18:31 +0100

    setup

---
+++ main.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    # img = np.asarray(Image.open("./input.png"))
+    # imgplot = plt.imshow(img)
+    # print(imgplot)
+    # imgplot.show()
+    pass
+    
+
+if __name__ == "__main__":
+    main()
